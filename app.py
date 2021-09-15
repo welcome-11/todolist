72e486a11c758d0b0789381b070e34ae85657285
welcome-11 <k.c-1920@hotmail.co.jp>
2021-09-15 15:05:07 +0900

    add csv read

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, date 
-import csv,os
+from io import StringIO
+import csv
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-data = []
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -64,20 +66,42 @@ def update(id):
         db.session.commit()
         return redirect('/')
 
+# フォーム表示
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template("home.html")
 
-@app.route("/csv")
-def csv():
-    csv_content = read_csv("AirPassengers")
-    for row in csv_content:
-        data.append(row[0])
-        print(data)
-    return render_template("csv.html",input_from_python= data) # templatesフォルダ内のcsv.htmlを表示する
+# アップロード機能
+@app.route('/upload', methods=['POST'])
+def upload():
+    try:
+        print(request.files)
+        #fileの取得（FileStorage型で取れる）
+        # https://tedboy.github.io/flask/generated/generated/werkzeug.FileStorage.html
+        fs = request.files['file']
+        # 下記のような情報がFileStorageからは取れる
+        app.logger.info('file_name={}'.format(fs.filename))
+        app.logger.info('content_type={} content_length={}, mimetype={}, mimetype_params={}'.format(
+        fs.content_type, fs.content_length, fs.mimetype, fs.mimetype_params))
+        # ファイルを保存
+        fs.save(fs.filename)
+        # アップしたファイルをインサートする
+        reading_csv(fs.filename)
+        return render_template("uploaded.html", data = data)
+    except:
+        return "ファイルがありません"
 
-def read_csv(filename):
-    csv_file = open("./csv/" + str(filename) + ".csv", "r", encoding="ms932", errors="", newline="" )
-    f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
-    #f = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
-    return f
+# CSVファイルを読み込む関数
+def reading_csv(filename):
+    data = []
+    with open(filename, encoding='cp932') as f:
+        reader = csv.reader(f)
+        header_ = next(csv.reader(f))
+        for row in reader:
+            tuples=(row[0], row[1], row[2], row[3], row[4], row[5])
+            print(tuples)
+            data.append(tuples,)
+        return data
 
 if __name__ == "__main__":
     # app.run(debug=True)
